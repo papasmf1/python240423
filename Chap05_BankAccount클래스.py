046bbc43c52bb57dabb30ca3a4a9570b8947ca22
papasmf1 <papasmf1@gmail.com>
2024-04-23 16:50:31 +0900

    클래스 연습4

--- Chap05_BankAccount클래스.py
+++ Chap05_BankAccount클래스.py
@@ -2,23 +2,27 @@
 #은행의 계정을 표현한 클래스 
 class BankAccount:
     def __init__(self, id, name, balance):
-        #인스턴스 멤버 변수 
-        self.id = id
-        self.name = name 
-        self.balance = balance 
+        #인스턴스 멤버 변수(내부에 이름 숨김) 
+        self.__id = id
+        self.__name = name 
+        self.__balance = balance 
     def deposit(self, amount):
-        self.balance += amount 
+        self.__balance += amount 
     def withdraw(self, amount):
-        self.balance -= amount
+        self.__balance -= amount
+    #선택한 블럭 주석: ctrl + / 
     def __str__(self):
-        return "{0}, {1}, {2}".format(self.id, 
-        self.name, self.balance)
+        return "{0}, {1}, {2}".format(self.__id, 
+        self.__name, self.__balance)
 
 #인스턴스 객체를 생성
 account1 = BankAccount(100, "전우치", 15000)
 account1.deposit(5000)
 account1.withdraw(3000)
 print(account1)
+#클래스 외부에서 접근 금지 
+print(account1.__balance)
+
 
 
 
