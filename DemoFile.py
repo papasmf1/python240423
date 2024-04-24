a9e3dc0a87b2a4866e2a319a45410b4cf98040b9
papasmf1 <papasmf1@gmail.com>
2024-04-24 16:35:05 +0900

    파일 연습 완료

--- DemoFile.py
+++ DemoFile.py
@@ -16,10 +16,20 @@ print(result)
 print("---readline()---")
 #다시 처음으로 돌아가서 읽기 
 f.seek(0)
-print(f.readline())
-print(f.readline())
+print(f.readline(), end="")
+print(f.readline(), end="")
 
+#전체를 읽어서 리스트 리턴 
+print("---readlines()---")
+f.seek(0)
+result = f.readlines()
+print(result)
 
+f.close() 
+
+#기존 파일에 첨부: append + read + write 
+f = open(r"c:\work\test2.txt", "a+", encoding="utf-8")
+f.write("추가 데이터\n")
 f.close() 
 
 
