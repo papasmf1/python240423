d659c09ed78fac4b1318ace480ba9b84d2c18a47
papasmf1 <papasmf1@gmail.com>
2024-04-25 10:36:40 +0900

    정규표현식 연습2

--- DemoRE.py
+++ DemoRE.py
@@ -2,10 +2,24 @@
 #정규표현식
 import re 
 
-result = re.search("[0-9]*th", "35th")
+#검색한 결과를 매칭오브젝트로 리턴 
+result = re.search("[0-9]*th", "  35th")
 print(result)
 print(result.group())
 
-result = re.match("[0-9]*th", "35th")
-print(result)
+# result = re.match("[0-9]*th", "  35th")
+# print(result)
+# print(result.group())
+
+print("---연도---")
+result = re.search("\d{4}", "올해는 2024년입니다.")
 print(result.group())
+
+print("---우편번호---")
+result = re.search("\d{5}", "우리동네는 51333")
+print(result.group())
+
+print("---단어 검색---")
+result = re.search("apple", "this is apple")
+print(result.group())
+
